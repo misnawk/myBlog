cbd0a29c622cd9144ad17448efe2b3a5aaa8b6dc
DESKTOP-M8E14FD\kms13 <kms13011@naver.com>
2025-05-27 03:35:51 +0900

    환경변수 전달..

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import{AuthModule} from './auth/auth.module';
@@ -10,14 +11,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST || '127.0.0.1',
-      port: parseInt(process.env.DB_PORT || '3306'),
-      username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || 'password',
-      database: process.env.DB_DATABASE || 'homeblog',
-      autoLoadEntities: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../../.env', // 프로젝트 루트의 .env 파일 경로
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+       // synchronize: true, // 개발환경에서만 사용
+      }),
+      inject: [ConfigService],
     }),
     AuthModule,
     UserModule
