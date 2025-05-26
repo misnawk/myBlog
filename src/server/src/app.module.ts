27f5eb15bffb296e6f6a83941ef640d72fdcbeba
DESKTOP-M8E14FD\kms13 <kms13011@naver.com>
2025-05-27 02:06:12 +0900

    no message

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -36,11 +36,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'mysql',
       host: process.env.DB_HOST || 'localhost',
       port: parseInt(process.env.DB_PORT || '3306'),
-      username: process.env.DB_USERNAME || 'minseok',
-      password: process.env.DB_PASSWORD || 'minseokpw',
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || '0000',
       database: process.env.DB_DATABASE || 'homeblog',
       autoLoadEntities: true,
-      synchronize: true, // 개발용만
+    //  synchronize: true, // 개발용만
     }),
     AuthModule,
     UserModule
