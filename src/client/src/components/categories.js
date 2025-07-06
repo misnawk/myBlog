0ff1930129ec28705e17434fdd3b5a76228f8115
DESKTOP-M8E14FD\kms13 <kms13011@naver.com>
2025-07-07 00:30:42 +0900

    카테고리 추가

--- src/client/src/components/categories.js
+++ src/client/src/components/categories.js
@@ -49,17 +49,44 @@ export const CATEGORIES = [
     color: '#48BB78'
   },
   {
-    id: '앱',
-    name: '앱',
-    description: '앱 개발, 앱 디자인',
-    color: '#4299E1'
+    id: '모바일',
+    name: '모바일 개발',
+    description: 'Android, iOS, React Native, Flutter',
+    color: '#3DDC84'
   },
   {
-    id: '주식',
-    name: '주식',
-    description: '투자, 시장 분석, 경제',
+    id: '시스템프로그래밍',
+    name: '시스템 프로그래밍',
+    description: '컴파일러, 링커, 저수준 프로그래밍',
+    color: '#805AD5'
+  },
+  {
+    id: 'DevOps',
+    name: 'DevOps/인프라',
+    description: 'Docker, K8s, CI/CD, 클라우드',
+    color: '#2D3748'
+  },
+  {
+    id: '알고리즘',
+    name: '알고리즘/자료구조',
+    description: '코딩테스트, PS, 자료구조',
     color: '#D69E2E'
+  },
+  {
+    id: 'CS기초',
+    name: 'CS 기초',
+    description: '컴퓨터구조, 운영체제, 이산수학', 
+    color: '#DD6B20'
+  },
+  {
+    id: '블록체인',
+    name: '블록체인/Web3',
+    description: '스마트 컨트랙트, DeFi, NFT',
+    color: '#319795'
   }
+
+  
+  
 ];
 
 // 카테고리 이름만 배열로 추출
